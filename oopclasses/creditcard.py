f1069ff53750bec2594a5ced0916cf942c6daadf
Amedi Imbira <155066628+Amedi-Imbira@users.noreply.github.com>
2024-04-21 10:33:37 +0300

    🔨 Exception handling in make_payment method

--- oopclasses/creditcard.py
+++ oopclasses/creditcard.py
@@ -9,32 +9,37 @@ class CreditCard:
       _balance: float = 0
       
       @property      
-      def customer(self):
+      def customer(self) -> str:
             return self._customer
       
       @property
-      def bank(self):
+      def bank(self) -> str:
             return self._bank
       
       @property
-      def account(self):
+      def account(self) -> str:
             return self._account
       
       @property
-      def limit(self):
+      def limit(self) -> int:
             return self._limit
       
       @property
-      def balance(self):
+      def balance(self) -> float:
             return self._balance
       
-      def charge(self, price):
+      def charge(self, price: float):
             if self._balance + price > self._limit:
                   return False
             else:
                   self._balance += price
                   
-      def make_payment(self, amount):
+      def make_payment(self, amount) -> None:
+            if not isinstance(amount, (int, float)):
+                  raise TypeError('Amount must be numerical')
+            elif amount < 0:
+                  raise ValueError('Amount cannot be negative')
+            
             self._balance -= amount
             
 cc = CreditCard('John Bowman', 'Carlifonia Savings', '2345 1232 4576 8787', 1000)
