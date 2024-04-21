d78b30049173b2603a634ed10adad80d3c849517
Amedi Imbira <155066628+Amedi-Imbira@users.noreply.github.com>
2024-04-21 18:51:41 +0300

    ✨ Substraction and Negate method added

--- oopclasses/vector.py
+++ oopclasses/vector.py
@@ -1,23 +1,43 @@
 class Vector:
-      def __init__(self, d):
-            self._coords = [0] * d
+      def __init__(self, d) -> None:
+            self._coords: list[int] = [0] * d
             
-      def __len__(self):
+      def __len__(self) -> int:
             return len(self._coords)
       
       def __getitem__(self, i):
             return self._coords[i]
       
-      def __setitem__(self, i, val):
+      def __setitem__(self, i, val) -> None:
             self._coords[i] = val
             
-      def __add__(self, other):
+      def __add__(self, other: list[int]) -> list[int]:
             if len(self) != len(other):
                   raise ValueError('Dimensions must be equal')
             
             result = Vector(len(self))
+            
             for j in range(len(self)):
                   result[j] = self[j] + other[j]
+            return result
+      
+      def __sub__(self, other: list[int]) -> list[int]:
+            if len(self) != len(other):
+                  raise ValueError('Dimensions must agree')
+            
+            result = Vector(len(self))
+            
+            for j in range(len(self)):
+                  result[j] = self[j] - other[j]
+                  
+            return result
+      
+      def __neg__(self):
+            result = Vector(len(self))
+            
+            for j in range(len(self)):
+                  result[j] = -self[j]
+            
             return result
       
       def __eq__(self, other):
@@ -30,4 +50,16 @@ class Vector:
             return '<' + str(self._coords)[1:-1] + '>'
       
 v1 = Vector(3)
-print(v1)
+v1[0] = 8
+v1[1] = 3
+v1[2] = 5
+# print(v1)
+
+v2 = Vector(3)
+v2[0] = 1
+v2[1] = 2
+v2[2] = 3
+
+print('Substract: ', v1 - v2)
+print('Add: ', v1 + v2)
+print('Negate: ', -v1)
